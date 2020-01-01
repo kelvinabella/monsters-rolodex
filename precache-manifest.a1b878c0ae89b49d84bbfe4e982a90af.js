d43159ad970e72b7523b8c9092648d1cbf12d8e2
Kelvin <abellavinkel@gmail.com>
2020-01-02 00:44:55 +0400

    Updates

---
+++ precache-manifest.a1b878c0ae89b49d84bbfe4e982a90af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "222f9d3eb66b0221c05cb18ad22ea31f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "15ebe253c5075113be14",
+    "url": "/monsters-rolodex/static/css/main.3aa49333.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "15ebe253c5075113be14",
+    "url": "/monsters-rolodex/static/js/main.62812812.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
